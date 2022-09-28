39de9220d8b3726d5f482a7d6bb5afc359e4f26d
OscarMoscar198 <osjacaso@gmail.com>
2022-09-27 22:36:56 -0500

    add

---
+++ vite-proyect/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <> 
+    <Header /> 
+    <Body />
+    <Footer />
+  </>
+);
